cd62d27ae161a3c6c63f4d750e34cf54c3e9dd3f
maarkeez <d.marquez.delgado.91@gmail.com>
2019-02-10 15:05:54 +0100

    Initial commit

---
+++ src/main/java/dmd/shared/resources/Application.java
@@ -0,0 +1,15 @@
+package dmd.shared.resources;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ImportResource;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
